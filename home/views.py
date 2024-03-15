ed1a522622b876297c332c2086c1321f21b57919
ifzal malook <ifzal.malook@gmail.com>
2024-03-15 16:24:02 +0000

    Working django now

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth import logout
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home/home.html'
+from django.contrib.auth import logout
