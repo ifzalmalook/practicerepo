27814565fddaf06435381f6244f4a3de31caa147
ifzal malook <ifzal.malook@gmail.com>
2024-03-16 22:36:49 +0000

    Add full_post view and styling for the full_project page

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import AddProject, Projects
+from . import views
 
 urlpatterns = [
     path('', AddProject.as_view(), name='add_project'),
-    path('projects', Projects.as_view(), name='projects')
+    path('projects', Projects.as_view(), name='projects'),
+    path('<slug:slug>/', views.full_project, name='full_project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Project
 from .forms import ProjectForm
+from django.utils.text import slugify
 
 # Create your views here.
 
@@ -29,9 +30,25 @@ class AddProject(LoginRequiredMixin, CreateView):
     template_name = 'projects/add_project.html'
     model = Project
     form_class = ProjectForm
-    succes_url = '/projects/'
+    success_url = '/projects/'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+
+        form.instance.slug = slugify(form.instance.title)
         messages.success(self.request, "Your project has been added!")
         return super(AddProject, self).form_valid(form)
+
+
+#view to generate full project page from the link on project listings
+def full_project(request, slug):
+    # Retrieve the project object using the provided slug
+    queryset = Project.objects.filter(slug=slug)
+    project = get_object_or_404(queryset, slug=slug)
+    
+    # Pass the project object to the template context
+    return render(
+        request,
+        "projects/full_project.html",
+        {"project": project}  # Use the correct variable name here
+    )
